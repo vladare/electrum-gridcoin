5cf636ee86199cc5336b49d3955134998ad8f358
paybee <dev@payb.ee>
2014-04-14 23:46:15 +0200

    added a second server, no thanks to moolah_

--- lib/network.py
+++ lib/network.py
@@ -8,6 +8,7 @@ DEFAULT_PORTS = {'t':'50001', 's':'50002', 'h':'8081', 'g':'8082'}
 
 DEFAULT_SERVERS = {
     'electrum.payb.ee': DEFAULT_PORTS,
+    'nodealpha.electrum-doge.org': DEFAULT_PORTS,
 }
 
 
